eda8e7e85354cb98f1040d0150628e778569a318
MatheusAllein <mtsallein@gmail.com>
2020-02-27 17:47:30 -0300

    Creating interface

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { Router } from 'react-router-dom';
+import theme from './themes/default';
 import Footer from './components/Footer';
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <h1>Partition Monitor</h1>
-      </div>
-      <Footer />
-    </>
+    <MuiThemeProvider theme={theme}>
+      <Router history={history}>
+        <Routes />
+        <Footer />
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
