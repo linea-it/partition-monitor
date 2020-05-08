ee11e39c95d470b26d9098fa63e8eeedd6bddbc5
Fernando <fernando_nicolau@outlook.com>
2020-05-08 10:48:07 -0300

    refactor: route adjustments and initial layout.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Route from './Route';
 import Server from '../pages/Server';
 import Duc from '../pages/Duc';
+import Main from '../pages/Main';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/duc.cgi" component={Duc} />
       <Route exact path="/:server" component={Server} />
-
-      <Route exact path="/">
+      <Route exact path="/" component={Main}/>
+      {/* <Route exact path="/">
         <Redirect to="/duc.cgi" />
-      </Route>
+      </Route> */}
     </Switch>
   );
 }
