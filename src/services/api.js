0cb073c7b02f6d812af794a54e9fbaf4a7b2099c
Fernando Silva <fernando_nicolau@outlook.com>
2020-06-16 12:00:32 -0300

    fix: changing the secondary color of the theme and removing fixed data

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 export const url = process.env.REACT_APP_API;
 axios.defaults.baseURL = url;
-let hash = btoa("fernando.silva:linea2019"); 
-axios.defaults.headers.common.Authorization = "Basic " + hash;
-
 
 export const getPartitionsByServer = server =>
   axios.get('/').then(res => res.data.filter(row => row.server === server));
