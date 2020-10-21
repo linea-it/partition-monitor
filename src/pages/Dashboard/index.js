58ddabdbec72624b15676cd8ee85afab393fdc27
Fernando Silva <fernando_nicolau@outlook.com>
2020-10-21 12:18:50 -0300

    feat: Pie chart layout adjustments.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 import { getServerHistory } from '../../services/api';
-import Typography from '@material-ui/core/Typography';
 import { megabytesToSize, megabytesToTerabytesGraph } from '../../services/math';
 import PiePlot from '../../components/Plot/PiePlot';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import styles from './styles';
 
 function Dashboard({setTitle}) {
@@ -13,23 +15,30 @@ function Dashboard({setTitle}) {
   const [plotData, setPlotData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  function arrayInsert (arr, server) {
-    let position = ['MS01', 'MS02', 'MS04','DESDB4','DESDB6','LUSTRE','FEROCKS','ALTIX','APOLLO', 'DTS'].indexOf(server.server.toUpperCase());
-    arr[position] = {
-      total: megabytesToSize(server.total_size),
-      used: megabytesToSize(server.total_use),
-      free: megabytesToSize(server.total_size - server.total_use),
+  function arrayInsert (server, mbGraph) {
+    setPlotData(prevState => ([...prevState , {
       title: server.server,
-      values: [megabytesToTerabytesGraph(server.total_size - server.total_use) , megabytesToTerabytesGraph(server.total_use)],
-      marker: {
-        colors: ['rgb(50,205,50)', 'rgb(175,0,0)']
+      dataPie: {
+        values: [megabytesToTerabytesGraph(server.total_size - server.total_use) , megabytesToTerabytesGraph(server.total_use)],
+        labels: ['Free:     '+ mbGraph.free, 'Used:    '+ mbGraph.used],
+        name: "seats",
+        hoverinfo :"label+percent+name",
+        hole: .4,
+        sort: false,
+        type: "pie",
+        marker: {
+          colors: ['rgb(50,205,50)', 'rgb(175,0,0)']
+        }
       },
-      labels: ['Free', 'Used'],
-      type: 'pie',
-      textinfo: 'label+percent',
-      hoverinfo: 'label+percent',
-      hole: .4,
-    }
+      dataTotal: {
+        values: [megabytesToTerabytesGraph(server.total_size)],
+        labels: ['Total:    '+ mbGraph.total],
+        name: "total",
+        visible: "legendOnly",
+        hole: .4,
+        type: "pie",
+      }
+    }]))
   }
 
   useEffect(() => {
@@ -39,18 +48,22 @@ function Dashboard({setTitle}) {
   useEffect(() => {
     getServerHistory().then(res => {
       // Get unique array of objects by a specific property
+      const serverName = ['MS01', 'MS02', 'MS04','DESDB4','DESDB6','LUSTRE','FEROCKS','ALTIX','APOLLO', 'DTS'];
       const uniqueServerEntries = res.data.filter((obj, pos, arr) =>
         arr.map(mapObj => mapObj.server)
         .indexOf(obj.server) === pos
       )
-      let orderedArray = [];
-      uniqueServerEntries.forEach ( (server, index) => {
-        arrayInsert(orderedArray, server);
-        if (uniqueServerEntries.length === index + 1) {
-          setPlotData(orderedArray);
-        };
+      serverName.forEach(name => {
+        const server = uniqueServerEntries.filter(obj => obj.server.toUpperCase() === name)[0];
+        if (server) {        
+          const mbGraph = {
+            total: megabytesToSize(server.total_size),
+            free: megabytesToSize(server.total_size - server.total_use),
+            used: megabytesToSize(server.total_use)
+          }
+          arrayInsert(server, mbGraph);
+        }
       });
-
       setLoading(false);
     });
   }, []);
@@ -58,16 +71,16 @@ function Dashboard({setTitle}) {
   return loading ? <CircularProgress className={classes.circularProgress} /> : (
     <Grid container spacing={3}>
       {plotData.map((data) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={data.title}>
-          <Typography variant="subtitle1" gutterBottom className={classes.labelPieChart}>
-            {`Total: ${data.total}`}<br />
-            {`Used: ${data.used}`}<br />
-            {`Free: ${data.free}`}<br />
-          </Typography>
-          <PiePlot
-            data={[data]}
-            title={data.title}
-          />
+        <Grid item xs={12} sm={6} xl={4} key={data.title}>
+          <Card className={classes.root}>
+            <CardHeader title={'Server - ' + data.title} />
+            <CardContent>
+              <PiePlot
+                data={data}
+                title={data.title}
+              />
+            </CardContent>
+          </Card>
         </Grid>
       ))}
     </Grid>
